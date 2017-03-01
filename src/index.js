0b51b4f2f5dca81ad9004808684a0adcb72f60d2
Juho Vepsalainen <bebraw@gmail.com>
2017-03-01 11:18:48 +0200

    Add project setup
    Derived from `webpack-defaults`. Good enough.

--- src/index.js
+++ src/index.js
@@ -4,36 +4,39 @@ const combinations = require('combinations');
 
 module.exports = function calculateMinimumFonts({
   browsers,
-  fonts = ['ttf', 'svg-fonts', 'woff', 'woff2']
+  fonts = ['ttf', 'svg-fonts', 'woff', 'woff2'],
 }) {
-  const supportedFonts = fonts.map(font => ({
-    browsers: caniuse.getSupport(font),
-    font
-  }));
-  const supportedBrowsers = getLowestVersions(browsers || browserslist());
-  const supportedFontsPerBrowser = Object.keys(supportedBrowsers).map(browserName => {
+  const supportedFonts = fonts.map((font) => {
+    return ({
+      browsers: caniuse.getSupport(font),
+      font,
+    });
+  });
+  const supportedBrowsers = getLowestVersions(browsers || browserslist());
+  const supportedFontsPerBrowser = Object.keys(supportedBrowsers).map((browserName) => {
     const browserVersion = supportedBrowsers[browserName];
 
     return {
       browser: browserName,
       supports: supportedFonts.map(({ browsers, font }) => (
         (browsers[browserName] && browsers[browserName].y <= browserVersion) && font
-      )).filter(a => a)
+      )).filter(a => a),
     };
   });
 
   return solveFonts(fonts, supportedFontsPerBrowser);
-}
+};
 
 // Adapted from babel-preset-env
 function getLowestVersions(browsers) {
   return browsers.reduce((all, browser) => {
-    const [browserName, browserVersion] = browser.split(" ");
-    const parsedBrowserVersion = parseInt(browserVersion);
+    const [browserName, browserVersion] = browser.split(' ');
+    const parsedBrowserVersion = parseInt(browserVersion, 10);
 
     if (!isNaN(parsedBrowserVersion)) {
       all[browserName] = Math.min(all[browserName] || Infinity, parsedBrowserVersion);
     }
+
     return all;
   }, {});
 }
@@ -41,13 +44,15 @@ function getLowestVersions(browsers) {
 // XXX: This can be abstracted (naming) since it's just a solver
 function solveFonts(allFonts, supportedFontsPerBrowser) {
   const minimumFonts = supportedFontsPerBrowser.map(({ supports }) => supports);
-  const alternatives = allFonts.map(font => (
+  const alternatives = allFonts.map((font) => {
+    return (
     {
       font,
-      matches: minimumFonts.map(minimum => minimum.indexOf(font) >= 0)
+      matches: minimumFonts.map(minimum => minimum.indexOf(font) >= 0),
     }
-  ));
-  const scoredCombinations = combinations(alternatives).map(combination => {
+    );
+  });
+  const scoredCombinations = combinations(alternatives).map((combination) => {
     const matches = combination.map(({ matches }) => matches).reduce((all, array) => (
       array.map((a, i) => a || all[i])
     ), []).filter(a => a);
